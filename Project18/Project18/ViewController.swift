0b60c8fad67c37540963536d80cefac1855f28c1
Oliwier Kasprzak <oliwier.kasprzak@icloud.com>
2022-11-14 11:49:12 +0100

    Add files via upload

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Oliwier Kasprzak on 27/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for i in 1...100 {
+            print("Got number \(i).")
+        }
+    }
+
+
+}
+
