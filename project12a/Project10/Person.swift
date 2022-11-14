2cf8593572dccb16f1faf997920c62718634dde9
Oliwier Kasprzak <oliwier.kasprzak@icloud.com>
2022-11-14 11:45:14 +0100

    Add files via upload

---
+++ project12a/Project10/Person.swift
@@ -0,0 +1,28 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by TwoStraws on 18/08/2016.
+//  Copyright © 2016 Paul Hudson. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+	var name: String
+	var image: String
+
+	init(name: String, image: String) {
+		self.name = name
+		self.image = image
+	}
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+}
