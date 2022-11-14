7d1244133df9738c3f0a66b0f880a3d6fb45262d
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2022-11-14 16:23:07 +0100

    Initial Commit

---
+++ Pairs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pairs
+//
+//  Created by Oliwier Kasprzak on 14/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
